df60b3e5fd86d88455ae0a0671e3633adf2a6624
MichalSzopa <szopqa96@gmail.com>
2017-10-09 20:43:55 +0200

    Made simple status system - saving each pin state to json file to be able to check which pin has which state

--- server/app.js
+++ server/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const router = require('./routers/switchesRoutes.js');
+const config = require('../client/src/config/config.js');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
 	// Website you wish to allow to connect
-	const clientAddr = 'http://192.168.1.100';
-	res.setHeader('Access-Control-Allow-Origin', clientAddr + ':3000');
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
 	// Allowed request methods
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
--- server/controllers/switchesController.js
+++ server/controllers/switchesController.js
@@ -1,17 +1,21 @@
+let pinSwitcher = require('../raspberry_controllers/pinSwitcher.js');
+let pinsStatus = require('../raspberry_controllers/pinsStatus/pinsStatus.js');
+
 function changeSwitchState(req,res) {
 
 	let pinNumber = req.params.pinNumber;
 
-	if( ! req.body )
+	if( ! req.body)
 		return res.sendStatus(400);
 
+	//Changing state of chosen pin
+	pinSwitcher.switchPin(pinNumber,req.body.pinShouldBeTurnedOn);
 	res.send(`PIN ${pinNumber} turned ${(req.body.pinShouldBeTurnedOn ? 'on' : 'off')}`);
-
 }
 
 function getSwitchesState(req,res) {
-	res.header("Access-Control-Allow-Origin","*");
-	res.send('GET response')
+	res.send('Get response');
+	console.log(pinsStatus.getPinsStatus());
 }
 
 module.exports = {
