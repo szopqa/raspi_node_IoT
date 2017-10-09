ea152cbff48e1c376bb653f96c7d39b85dcffcae
MichalSzopa <szopqa96@gmail.com>
2017-10-09 21:05:56 +0200

    New syntax changes

--- server/controllers/switchesController.js
+++ server/controllers/switchesController.js
@@ -1,16 +1,16 @@
-let pinSwitcher = require('../raspberry_controllers/pinSwitcher.js');
-let pinsStatus = require('../raspberry_controllers/pins_status/pinsStatus.js');
+var pinSwitcher = require('../raspberry_controllers/pinSwitcher.js');
+var pinsStatus = require('../raspberry_controllers/pins_status/pinsStatus.js');
 
 function changeSwitchState(req,res) {
 
-	let pinNumber = req.params.pinNumber;
+	var pinNumber = req.params.pinNumber;
 
 	if( ! req.body)
 		return res.sendStatus(400);
 
 	//Changing state of chosen pin
 	pinSwitcher.switchPin(pinNumber,req.body.pinShouldBeTurnedOn);
-	res.send(`PIN ${pinNumber} turned ${(req.body.pinShouldBeTurnedOn ? 'on' : 'off')}`);
+	res.send('PIN ' + pinNumber + ' turned ' + (req.body.pinShouldBeTurnedOn ? 'on' : 'off'));
 }
 
 function getSwitchesState(req,res) {
