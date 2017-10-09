46f8d4444b751a1370ae5ab6083bde06b5764240
MichalSzopa <szopqa96@gmail.com>
2017-10-09 12:45:11 +0200

    Added middleware which enables CORS requests

--- server/app.js
+++ server/app.js
@@ -4,9 +4,22 @@ const bodyParser = require('body-parser');
 const router = require('./routers/switchesRoutes.js');
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+	// Website you wish to allow to connect
+	const clientAddr = 'http://192.168.1.100';
+	res.setHeader('Access-Control-Allow-Origin', clientAddr + ':3000');
 
+	// Allowed request methods
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	// Allowed request headers
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
 
+//Requests router
 router.switchesRoutes(app);
 
 
