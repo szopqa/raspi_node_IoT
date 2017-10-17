3482d1ccf4f000d90178d6a6b2056ac8033c2ab8
MichalSzopa <szopqa96@gmail.com>
2017-10-18 00:30:57 +0200

    Added video component displaying stream from raspberry pi cam

---
+++ client/src/components/video/Video.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import config from '../../config/config';
+
+//Static photo used for testing
+import testingPhoto from '../images/test.jpg'
+
+import './Video.css';
+
+class Video extends React.Component {
+
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+
+		let raspberryStream = 'http://'+config.serverIP+':8081/';
+
+		return(
+			<div className="video-viewer">
+				<img src={raspberryStream}  id="video" alt="Loading view ..."/>
+			</div>
+		);
+	}
+
+}
+
+export default Video;
