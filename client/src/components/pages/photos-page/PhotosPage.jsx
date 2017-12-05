151c6e1465d0a74c1fd2dac42e12344e20be39aa
szopqa <szopqa96@gmail.com>
2017-12-05 22:53:02 +0100

    Minor config changes

--- client/src/components/pages/photos-page/PhotosPage.jsx
+++ client/src/components/pages/photos-page/PhotosPage.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import {Grid,Col,Row} from 'react-bootstrap';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import axios from 'axios';
+import _ from 'lodash';
+
 import Notifications from './notifications/Notifications.jsx'
 import Photos from './photos/Photos.jsx';
+import config from '../../../config/config.js';
+
 
 import './PhotosPage.css';
-
-
 
 class PhotosPage extends React.Component{
 
 	constructor(props){
 		super (props);
+		this.state = {
+			fetchedPhotos :[],
+			fetchedNotifications : []
+		}
+	}
+
+	componentDidMount (){
+		axios.get(`http://${config.serverAddress}/notifications`)
+			.then((response) => {
+				let notifications = [];
+				let photos = [];
+
+				//extracting data from response
+				response.data.map((element) => {
+					notifications.push(_.omit(element,['image']));
+					photos.push(element.attachment);
+				});
+
+				this.setState({
+                    fetchedPhotos : photos,
+                    fetchedNotifications : notifications
+				})
+			})
+			.catch((err) => {
+				console.log(err);
+			})
 	}
 
 	render(){
@@ -22,10 +51,14 @@ class PhotosPage extends React.Component{
 						<Grid>
 							<Row>
 								<Col lg={4} sm={12}>
-									<Notifications/>
+									<Notifications
+										fetchedNotifications = {this.state.fetchedNotifications}
+									/>
 								</Col>
 								<Col lg={8} sm={12}>
-									<Photos/>
+									<Photos
+										fetchedPhotos = {this.state.fetchedPhotos}
+									/>
 								</Col>
 							</Row>
 						</Grid>
--- server/raspberry/cameraConfig.js
+++ server/raspberry/cameraConfig.js
@@ -1,16 +1,14 @@
 var parameters = {
     mode : "photo",
     output : changeFilename(),
-    width : 640,
-    height : 480,
-    quality : 100,
+    width : 400,
+    height : 300,
     encoding : 'jpg',
 };
 
 function changeFilename() {
     var fileName = new Date().toISOString().slice(0,19).replace(/:/g,"-");
     return photoPath = './photos/' + fileName + '.jpg';
-
 }
 
 module.exports = {
