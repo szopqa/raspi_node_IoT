7ee5b0a53c304ab69ecc33152159aeb90cc61047
szopqa <szopqa96@gmail.com>
2017-12-05 23:41:22 +0100

    fixed untracked files

---
+++ server/config/secrets.js
@@ -0,0 +1,8 @@
+module.exports = {
+    key : 'clear_database',
+    cloudinaryData:{
+        cloud_name: 'dtincuylz',
+        api_key: '615594361437889',
+        api_secret: 'hWXFz6o1b-cR_pthF3J9wea2RYU'
+    }
+};
